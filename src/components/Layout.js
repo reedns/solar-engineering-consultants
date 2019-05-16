5e2adca9ddef897f0891a090175465075379f5e9
Reed Nelson-Saunders <reedns@hotmail.com>
2019-05-16 11:07:44 -0700

    change favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,28 +15,10 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
-          type="image/png"
-          href="/img/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-16x16.png"
-          sizes="16x16"
+          href="/img/favicon.ico"
         />
 
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
