0056b32389e5412df3fb87ec13e19d3a9cc690c4
Reed Nelson-Saunders <reedness3000@gmail.com>
2021-04-06 20:03:43 -0700

    More cleanup

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,6 @@ exports.createPages = ({ actions, graphql }) => {
               slug
             }
             frontmatter {
-              tags
               templateKey
             }
           }
@@ -36,7 +35,6 @@ exports.createPages = ({ actions, graphql }) => {
       const id = edge.node.id
       createPage({
         path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
